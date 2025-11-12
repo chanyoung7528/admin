e567a0df7261c4648b1ccd16001b307b65ec6d17
suk <suk@mz.co.kr>
2025-11-12 14:07:32 +0900

    chore: update TypeScript configuration and remove unused files; adjust linting commands in pre-commit hook

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -35,6 +35,9 @@ export default tseslint.config(
           varsIgnorePattern: '^_',
         },
       ],
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', fixStyle: 'inline-type-imports' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'react-hooks/exhaustive-deps': 'warn',
     },
   }
 );
--- packages/shared/src/components/layouts/sidebar/TeamSwitcher.tsx
+++ packages/shared/src/components/layouts/sidebar/TeamSwitcher.tsx
@@ -23,6 +23,10 @@ export function TeamSwitcher({ teams }: TeamSwitcherProps) {
   const { isMobile } = useSidebar();
   const [activeTeam, setActiveTeam] = React.useState(teams[0]);
 
+  if (!activeTeam) {
+    return null;
+  }
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
